f9a4f5b9f629f5303f4df173c9cff3f8a07fa660
Dion Earle <dion.earle@gmail.com>
2022-11-18 14:30:01 +1100

    Allowing extra arguments and ignoring node modules

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ To setup in your project, add the following to `.pre-commit-config.yaml`:
 ```yaml
 repos:
   - repo: https://github.com/dionearle/strip-boto-data
-    rev: 1.0.0
+    rev: 1.0.1
     hooks:
       - id: strip-boto-data
+      - args: ['s3'] # Optional: If you want to manually specify additional boto3 services to include, add them here
 ```
 
 as well as updating your `serverless.yml` to include the following:
--- strip_boto_data/main.py
+++ strip_boto_data/main.py
@@ -1,18 +1,24 @@
 import re
+from fnmatch import fnmatch
 from glob import iglob
+from sys import argv
 
 
 def main():
     boto_imports = ["*.json", "ssm"]
+    boto_imports.extend(argv[1:])
+
     for filename in iglob("**/*.py", recursive=True):
+        if fnmatch(filename, "node_modules/*"):
+            continue
         with open(filename) as f:
             file_content = "".join([line.strip() for line in f.readlines()])
             if re.search(r"boto3", file_content):
-                services = re.findall(
+                services_used = re.findall(
                     r"(?:resource|client)\((?:service_name=|)['\"](.*?)['\"]", file_content
                 )
-                if services:
-                    boto_imports.extend(services)
+                if services_used:
+                    boto_imports.extend(services_used)
 
     boto_imports.sort()
     import_str = "|".join(list(dict.fromkeys(boto_imports)))
